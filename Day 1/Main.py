b3b37d09fb6f63f4d0d906dfb1b5b807f128c4eb
Rudraksh Sharma <rudr.sh08@gmail.com>
2025-08-01 13:13:14 +0530

    Main.py

---
+++ Day 1/Main.py
@@ -0,0 +1,10 @@
+# Greetings
+print("Welcome to the Band Name Generator.")
+#Name of city
+print("What's name of the city you grew up in? ")
+city = input()
+#Name of pet
+print("What's your pet's name? ")
+pet = input()
+#name of band
+print("Your band name could be " + city + " " + pet)

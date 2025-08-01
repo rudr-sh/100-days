73e6b6ef50a96d4f77dc867fa19c3de29284f879
Rudraksh Sharma <rudr.sh08@gmail.com>
2025-08-01 13:14:21 +0530

    Main.py

---
+++ Day 2/Main.py
@@ -0,0 +1,7 @@
+#ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ
+
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in Kg: ")
+
+BMI = (float(weight)/(float(height) ** 2))
+print("Your BMI is : " + str(round(BMI)))

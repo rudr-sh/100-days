5d841636ac5998aea3c094f9cb71d5be0b2aee2b
Rudraksh Sharma <rudr.sh08@gmail.com>
2025-08-02 13:49:33 +0530

    Main.py

---
+++ Day 3/Main.py
@@ -0,0 +1,9 @@
+#Don't change the code below
+print("Hello this program will tell you if a number is odd or even.")
+number = int(input("What number do you want to choose? "))
+#Write code from here
+
+if (number % 2) == 0:
+    print("Your Number is even. ")
+else:
+    print("Your number is odd") 

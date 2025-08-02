818b2b1eacaae3a7dbaab49223dc91588abab7e1
Rudraksh Sharma <rudr.sh08@gmail.com>
2025-08-02 13:48:38 +0530

    Day 3/Main.py

--- Day 3/Main.py
+++
@@ -1,8 +0,0 @@
-#Don't change the code below
-number = int(input("What number do you want to choose? "))
-#Write code from here
-
-if (number % 2) == 0:
-    print("Your Number is even. ")
-else:
-    print("Your number is odd") 
